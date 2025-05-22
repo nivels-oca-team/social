21c556631a63b6f56d18e4f8112a5c800e83714a
nivels-oca-team <ocateam@nivels.ch>
2025-05-22 16:54:18 +0200

    [IMP] migration from 15.0 to 17.0

---
+++ outgoing_email_by_model/tests/models.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class ModelWithMail(models.Model):
+    _name = "model.with.mail"
+    _inherit = ["mail.thread"]
+    _description ="Model With Mail"
+
+    partner_id = fields.Many2one("res.partner")
